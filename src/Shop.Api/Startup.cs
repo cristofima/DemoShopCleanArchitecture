23518170b074eb14d580f1b8aeba692e13153899
Cristopher Coronado Moreira <cristofima@hotmail.com>
2019-12-25 16:38:41 -0500

    Actualización de .NET Core 2.2 a 3.1

--- src/Shop.Api/Startup.cs
+++ src/Shop.Api/Startup.cs
@@ -2,11 +2,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Shop.Api.Middlewares;
 using Shop.Core.DTO;
 using Shop.Core.Interfaces;
@@ -18,7 +19,6 @@ using Shop.Infrastructure.Data.Contexts;
 using Shop.Infrastructure.Data.Identity;
 using Shop.Infrastructure.Data.Repositories;
 using Shop.Infrastructure.Services;
-using Swashbuckle.AspNetCore.Swagger;
 using System;
 using System.IO;
 using System.Reflection;
@@ -81,7 +81,7 @@ namespace Shop.Api
                    };
                });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddControllers();
 
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped<IUserRepository, UserRepository>();
@@ -98,7 +98,7 @@ namespace Shop.Api
             // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "Shop API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Shop API", Version = "v1" });
 
                 // Set the comments path for the Swagger JSON and UI.
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
@@ -108,30 +108,35 @@ namespace Shop.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+
+            app.UseHttpsRedirection();
 
             app.UseMiddleware<RequestHandlerMiddleware>();
 
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
 
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shop API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Basic Shop API REST");
             });
 
+            app.UseRouting();
+
             app.UseAuthentication();
-            app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
